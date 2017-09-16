17aa11432b38e3204002f98d889a353cd08a2b2a
Jeff <Jeff@asus-jeff>
2017-09-16 16:40:07 -0500

    User create , validation

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import express from 'express';
 import constants from './config/constants';
 import './config/database';
 import middlewaresConfig from './config/middlewares';
+import apiRoutes from './modules';
 
 const app = express();
 
@@ -12,6 +13,8 @@ middlewaresConfig(app);
 app.get('/', (req, res) => {
   res.send('Hello world');
 });
+
+apiRoutes(app);
 
 app.listen(constants.PORT, err => {
   if (err) {
